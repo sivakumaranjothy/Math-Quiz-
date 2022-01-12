6d2fef27a402b5c6de3e2e515c77b1b0a83dd225
sivakumaranjothy <80862492+sivakumaranjothy@users.noreply.github.com>
2022-01-11 20:19:11 -0500

    Add files via upload

---
+++ quiz_game_login.js
@@ -0,0 +1,8 @@
+function addUser()
+{
+    player1_name=document.getElementById("Player1_input").value;
+    player2_name=document.getElementById("Player2_input").value;
+    localStorage.setItem("player1_name",player1_name);
+    localStorage.setItem("player2_name",player2_name);
+    window.location="quiz_game_page.html";
+}
